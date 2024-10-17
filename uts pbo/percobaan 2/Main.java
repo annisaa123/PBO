d30cea7d0c9967d55d0257b387069aa8eef3e4e1
annisaa_ <144087697+annisaa123@users.noreply.github.com>
2024-10-17 23:32:32 +0700

    Delete uts pbo directory

--- uts pbo/percobaan 2/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Pascasarjana p = new Pascasarjana("Annisa", 20, "2341760032", "Pengembangan AI");
-        p.perkenalan();
-        p.belajar();
-        p.penelitian();
-    }
-    
-}
